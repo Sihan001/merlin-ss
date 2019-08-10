66c3865cb526ba28bba037ee5df900f13024fa0c
Sihan <wolf.mb@gmail.com>
2019-08-10 18:52:27 +0800

    update gfwlist.conf

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 ## Shadowsocks for Merlin 华硕路由梅林固件
 
-------
 
 非原创，提取 https://github.com/hq450/fancyss_history_package 科学上网插件。
 
 #### 机型支持
 
- 目前只测试RT-AC88U merlin固件，理论与ac88u同芯片
+ **华硕armv7系列** merlin固件
+
+- 华硕 RT-AC68U
+- 华硕 RT-AC66U-B1
+- 华硕 RT-AC1900
+- 华硕 RT-AC1900P
+- 华硕 RT-AC87U
+- 华硕 RT-AC88U
+- 华硕 RT-AC3100
+- 华硕 RT-AC3200
+- 华硕 RT-AC5300
 
 ------
 
-**使用**
+#### 说明
 
-目前支持 gfwlist 白单名、chnroute 中国白单名、全局三种代理模式。
+**代理方式**
+
+> 目前支持 gfwlist 白单名、chnroute 中国白单名、全局三种代理模式。
 
 - gfwlist 白单名，只要域名在名单内及配置了黑名单都走代理模式。
 - chnroute 白单名，只要ip不在chnroute 内及黑名单都走代理模式。
@@ -20,38 +31,51 @@
 
 **DNS 污染**
 
-目前支持3种解决方案：
+> 目前支持3种解决方案：
 
 1. chinaDNS，国内域名走国内DNS；国外域名走国外DNS；但不稳定，chinaDNS老挂。
-2. dns2socks，通过ss 服务器tcp转发。
-3. ss-tunnle，通过ss 服务器upd转发，需要服务器支持。
+2. dns2socks，通过 ss 服务器 tcp 转发。
+3. ss-tunnle，通过 ss 服务器 upd 转发，需要服务器支持。
 
-**config 配置**
+**config 配置说明**
 
 1. blacklist.txt，黑名单；无论那种模式，黑名单的IP都通过代理。
-2. chnroute.txt，中国白名单；
-3. dnsproxyip.txt，国外dns服务器；不设置以 8.8.8.8为主。
-4. gfwlist.conf，国外白名单；
-5. serverlist.txt，ss服务器列表，支持域名；会自动选择最快的服务器连接。
-6. ss.json，ss服务器配置。
-7. ssdns.json，ss-tunnle 为 DNS 时ss服务器配置。
+2. chnroute.txt，中国白名单。
+3. dnsproxyip.txt，纯净 dns 服务器；不设置默认 8.8.8.8 。
+4. gfwlist.conf，白名单；可在线更新。
+5. serverlist.txt，ss 服务器列表，支持域名；自动选择最快的服务器连接。
+6. ss.json，ss 服务器配置，服务器列表配置均需要一样。
+7. ssdns.json，ss-tunnle 为 DNS 时 ss 服务器配置。
 8. whitelist.txt，白名单，任何时候都不通过代理。
 
-**参数**
+**使用**
 
-./ss.sh [command]
+sh ss.sh [optiong]
 
-| start       | 以配置文件设置启动服务     |
+| 命令        | 描述                       |
 | ----------- | -------------------------- |
+| start       | 以配置文件设置启动服务     |
 | restart     | 以配置文件设置重新启动服务 |
-| stop        | 结束服务                   |
+| stop        | 关闭服务                   |
 | flush_nat   | 清空转发规则表             |
 | update_rule | 在线更新gfwlist            |
 
-| -h                      | 显示帮助                       |
-| ----------------------- | ------------------------------ |
-| -d <dns_mode>           | 设置dns污染方案                |
-| -m <proxy_mode>         | 设置代理模式                   |
-| -s <ss_server_ip>       | 指定ss服务器地址，不再自动选择 |
-| -n <dns_server_ip:port> | 指定纯净 dns                   |
+| 参数                     | 描述                             |
+| ------------------------ | -------------------------------- |
+| -h                       | 显示帮助                         |
+| -d <dns_mode>            | 设置dns污染方案                  |
+| -m <proxy_mode>          | 设置代理模式                     |
+| -s <ss_server_ip>        | 强制指定ss服务器地址，不自动选择 |
+| -n \<dns_server_ip:port> | 强制指定纯净 dns 服务器          |
 
+ex:
+
+```shell
+sh ./ss.sh start
+sh ./ss.sh restart
+sh ./ss.sh stop
+sh ./ss.sh flush_nat
+sh ./ss.sh update_rule
+sh ./ss.sh -h
+ss ./ss.sh -d 1 -m 2 -s xx.xx.xx.xx -n 114.114.114.114:53
+```
--- ss.sh
+++ ss.sh
@@ -8,7 +8,7 @@ alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
 # 0 global
 # 1 auto, gfwlist 
 # 2 chnroute, china ip
-PROXY_MODE=2
+PROXY_MODE=1
 
 # dns mode
 # 0 dns2sock
@@ -49,6 +49,25 @@ ip_prefix_hex=`nvram get lan_ipaddr | awk -F "." '{printf ("0x%02x", $1)} {print
 
 
 # function
+
+_green() {
+    printf '\033[1;31;32m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_red() {
+    printf '\033[1;31;31m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_yellow() {
+    printf '\033[1;31;33m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
 set_lock(){
 	exec 1000>"$LOCK_FILE"
 	flock -x 1000
@@ -67,37 +86,43 @@ set_ulimit(){
 kill_process(){
 	ssredir=`pidof ss-redir`
 	if [ -n "$ssredir" ];then 
-		echo_date 关闭ss-redir进程...
+		_red "关闭ss-redir进程..."
+		echo 
 		killall ss-redir >/dev/null 2>&1
 	fi
 	
 	sstunnel=`pidof ss-tunnel`
 	if [ -n "$sstunnel" ];then 
-		echo_date 关闭ss-tunnel进程...
+		_red "关闭ss-tunnel进程..."
+		echo
 		killall ss-tunnel >/dev/null 2>&1
 	fi
 
 	chinadns=`pidof chinadns1`
 	if [ -n "$chinadns" ];then 
-		echo_date 关闭chinadns进程...
+		_red "关闭chinadns进程..."
+		echo
 		killall chinadns1 >/dev/null 2>&1
 	fi
 	
 	haveged_process=`pidof haveged`
 	if [ -n "$haveged_process" ];then 
-		echo_date 关闭haveged进程...
+		_red "关闭haveged进程..."
+		echo
 		killall haveged >/dev/null 2>&1
 	fi
 
 	sslocal=`pidof ss-local`
 	if [ -n "$sslocal" ];then 
-		echo_date 关闭ss-local进程...
+		_red "关闭ss-local进程..."
+		echo
 		killall ss-local >/dev/null 2>&1
 	fi
 
 	dns2socks=`pidof dns2socks`
 	if [ -n "$dns2socks" ];then 
-		echo_date 关闭dns2socks进程...
+		_red "关闭dns2socks进程..."
+		echo
 		killall dns2socks >/dev/null 2>&1
 	fi
 }
@@ -112,10 +137,13 @@ get_server(){
 			#cat $SHAD_CFG_PATH/serverlist.txt | while read server
 			while read server
 			do
-				echo_date "测试[$server]速度..."
+				_green "测试"
+				_yellow "[$server]"
+				_green "速度..."
+				echo 
 				info=`ping -c 2 "$server" | awk 'NR==3{print}' | awk '{print $4,$7}'`
 				if [ -z "$info" ]; then
-					echo_date 跳过空记录
+					_yellow "跳过空记录"
 				else
 					time=`echo $info | awk -F '[=]' '{print $2}'`
 					time1=`echo $time | awk -F '[.]' '{print $1}'`
@@ -132,18 +160,23 @@ get_server(){
 							fi
 						fi
 						#echo_date 当前服务器: $SHAD_SERVER_IP
-						echo_date 响应时间: $time
+						_green "响应时间: "
+						_yellow "$time"
+						echo 
 					fi
 				fi
 			done < $SHAD_CFG_PATH/serverlist.txt
 		fi
 	fi
 
-	echo_date 选用SS服务器: $SHAD_SERVER_IP
+	_green "选用SS服务器: "
+	_yellow "$SHAD_SERVER_IP"
+	echo
 }
 
 close_in_five(){
-	echo_date "插件将在5秒后自动关闭！！"
+	_red "插件将在5秒后自动关闭！！"
+	echo
 	sleep 1
 	echo_date 5
 	sleep 1
@@ -156,8 +189,8 @@ close_in_five(){
 	echo_date 1
 	sleep 1
 	echo_date 0
-	echo_date "插件已关闭！！"
-	echo_date ======================= 梅林固件 - 【科学上网】 ========================
+	_red "插件已关闭！！"
+	echo 
 	unset_lock
 	exit
 }
@@ -166,11 +199,16 @@ close_in_five(){
 detect(){
 	# 检测jffs2脚本是否开启，如果没有开启，将会影响插件的自启和DNS部分（dnsmasq.postconf）
 	if [ "`nvram get jffs2_scripts`" != "1" ];then
-		echo_date "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-		echo_date "+     发现你未开启Enable JFFS custom scripts and configs选项！     +"
-		echo_date "+    【软件中心】和【科学上网】插件都需要此项开启才能正常使用！！         +"
-		echo_date "+     请前往【系统管理】- 【系统设置】去开启，并重启路由器后重试！！      +"
-		echo_date "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		_red "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		echo
+		_red "+     发现你未开启Enable JFFS custom scripts and configs选项！     +"
+		echo
+		_red "+    【软件中心】和【科学上网】插件都需要此项开启才能正常使用！！         +"
+		echo
+		_red "+     请前往【系统管理】- 【系统设置】去开启，并重启路由器后重试！！      +"
+		echo
+		_red "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+		echo
 		close_in_five
 	fi
 	
@@ -187,14 +225,16 @@ detect(){
 
 
 restore_conf(){
-	echo_date 删除ss相关配置文件...
+	_red "删除ss相关配置文件..."
+	echo
 	rm -rf /jffs/configs/dnsmasq.conf.add
 	rm -rf /jffs/configs/chnroute.txt
 }
 
 restart_dnsmasq(){
 	# Restart dnsmasq
-	echo_date 重启dnsmasq服务...
+	_yellow "重启dnsmasq服务..."
+	echo
 	service restart_dnsmasq >/dev/null 2>&1
 }
 
@@ -229,7 +269,8 @@ create_dnsmasq_conf(){
 
 	if [ $PROXY_MODE -eq 2 -o $DNS_MODE -eq 1 ]; then
 		if [ -f /jffs/configs/chnroute.txt ]; then
-			echo_date chnroute已生成
+			_yellow "chnroute已生成"
+			echo
 		else
 			cat $SHAD_CHNROUTE >> /jffs/configs/chnroute.txt
 		fi
@@ -238,7 +279,8 @@ create_dnsmasq_conf(){
 
 
 flush_nat(){
-	echo_date 清除iptables规则和ipset...
+	_yellow "清除iptables规则和ipset..."
+	echo
 	# flush rules and set if any
 	nat_indexs=`iptables -nvL PREROUTING -t nat |sed 1,2d | sed -n '/SHADOWSOCKS/='|sort -r`
 	for nat_index in $nat_indexs
@@ -276,14 +318,16 @@ load_module(){
 	xt=`lsmod | grep xt_set`
 	OS=$(uname -r)
 	if [ -f /lib/modules/${OS}/kernel/net/netfilter/xt_set.ko ] && [ -z "$xt" ];then
-		echo_date "加载xt_set.ko内核模块！"
+		_yellow "加载xt_set.ko内核模块！"
+		echo
 		insmod /lib/modules/${OS}/kernel/net/netfilter/xt_set.ko
 	fi
 }
 
 # create ipset rules
 create_ipset(){
-	echo_date 创建ipset名单
+	_green "创建ipset名单"
+	echo
 	ipset -! create white_list nethash && ipset flush white_list
 	ipset -! create gfwlist nethash && ipset flush gfwlist
 	ipset -! create black_list nethash && ipset flush black_list
@@ -300,7 +344,8 @@ create_ipset(){
 
 start_haveged(){
 	bin=$SHAD_BIN_PATH/haveged
-	echo_date "启动haveged，为系统提供更多的可用熵！"
+	_yellow "启动haveged，为系统提供更多的可用熵！"
+	echo
 	$bin -w 1024 >/dev/null 2>&1
 }
 
@@ -310,8 +355,10 @@ start_ss_redir(){
 	
 	bin=$SHAD_BIN_PATH/ss-redir
 	# tcp udp go ss
-	echo_date $bin的 tcp 走$bin.
-	echo_date $bin的 udp 走$bin.
+	_yellow "$bin的 tcp 走$bin."
+	echo
+	_yellow "$bin的 udp 走$bin."
+	echo
 
 	# echo_date 选用SS服务器: $SHAD_SERVER_IP
 	if [ "$SHAD_SERVER_IP" = "0" ]; then
@@ -319,13 +366,14 @@ start_ss_redir(){
 	else
 		$bin -s $SHAD_SERVER_IP -c $SHAD_CFG -u -f /var/run/shadowsocks.pid >/dev/null 2>&1
 	fi
-	echo_date $bin 启动完毕！.
+	_yellow "$bin 启动完毕！."
 }
 
 start_sslocal(){
 
 	bin=$SHAD_BIN_PATH/ss-local
-	echo_date 开启ss-local，提供socks5代理端口：23456
+	_yellow "开启ss-local, 提供socks5代理端口: 23456"
+	echo
 
 	# echo_date 选用服务器: $SHAD_SERVER_IP
 	if [ "$SHAD_SERVER_IP" = "0" ]; then
@@ -334,7 +382,8 @@ start_sslocal(){
 		$bin -s $SHAD_SERVER_IP -l 23456 -c $SHAD_CFG -u -f /var/run/sslocal1.pid >/dev/null 2>&1
 	fi
 
-	echo_date $bin 启动完毕！.
+	_yellow "$bin 启动完毕！."
+	echo
 }
 
 start_dns(){
@@ -353,7 +402,8 @@ start_dns(){
 
 	# chinaDNS
 	if [ $DNS_MODE -eq 1 ]; then
-		echo_date 开启chinaDNS，用于dns解析...
+		_yellow "开启chinaDNS，用于dns解析..."
+		echo
 		bin=$SHAD_BIN_PATH/chinadns1
 		if [ -f /jffs/configs/chnroute.txt ]; then
 			$bin -p $DNS_PORT -s $CDN,$DNS -d -c /jffs/configs/chnroute.txt &
@@ -362,14 +412,16 @@ start_dns(){
 		fi
 	elif [ $DNS_MODE -eq 2 ]; then
 		# ss-tunnel
-		echo_date 开启ss-tunnel，用于dns解析...
+		_yellow "开启ss-tunnel，用于dns解析..."
+		echo
 		bin=$SHAD_BIN_PATH/ss-tunnel
 		$bin -c $SHAD_DNS_CFG -l $DNS_PORT -L $DNS -u -f /var/run/sstunnel.pid >/dev/null 2>&1
 	else
 		# dns2sock
 		start_sslocal
 		bin=$SHAD_BIN_PATH/dns2socks
-		echo_date 开启dns2socks，用于dns解析...
+		_yellow "开启dns2socks，用于dns解析..."
+		echo
 		nohup $bin 127.0.0.1:23456 "$DNS" 127.0.0.1:$DNS_PORT > /dev/null 2>&1 &
 	fi 
 }
@@ -442,7 +494,8 @@ get_action_chain() {
 
 apply_nat_rules(){
 	#----------------------BASIC RULES---------------------
-	echo_date 写入iptables规则到nat表中...
+	_yellow "写入iptables规则到nat表中..."
+	echo
 	# 创建SHADOWSOCKS nat rule
 	iptables -t nat -N SHADOWSOCKS
 	
@@ -485,13 +538,15 @@ load_nat(){
 	do
 		i=$(($i-1))
 		if [ "$i" -lt 1 ];then
-			echo_date "错误：不能正确加载nat规则!"
+			_red "错误：不能正确加载nat规则!"
+			echo
 			close_in_five
 		fi
 		sleep 1
 		nat_ready=$(iptables -t nat -L PREROUTING -v -n --line-numbers|grep -v PREROUTING|grep -v destination)
 	done
-	echo_date "加载nat规则!"
+	_yellow "加载nat规则!"
+	echo
 	
 	add_white_black_ip
 	apply_nat_rules
@@ -499,9 +554,10 @@ load_nat(){
  
 
 apply_ss(){
-	echo_date ======================= 梅林固件 - 【科学上网】 ========================
-	echo_date
-	echo_date ------------------------- 启动【科学上网】 -----------------------------
+	_yellow "======================= 梅林固件 - 【科学上网】 ========================"
+	echo
+	_yellow "------------------------- 启动【科学上网】 -----------------------------"
+	echo
 	kill_process
 	detect
 	get_server
@@ -516,65 +572,76 @@ apply_ss(){
 	start_dns
 	load_nat
 	restart_dnsmasq
-	echo_date ------------------------ 【科学上网】 启动完毕 ------------------------
+	_green "------------------------ 【科学上网】 启动完毕 ------------------------"
+	echo
 }
 
 disable_ss(){
-	echo_date ======================= 梅林固件 - 【科学上网】 ========================
-	echo_date
-	echo_date ------------------------- 关闭【科学上网】 -----------------------------
+	_yellow "======================= 梅林固件 - 【科学上网】 ========================"
+	echo
+	_yellow "------------------------- 关闭【科学上网】 -----------------------------"
+	echo
 	kill_process
 	restore_conf
 	restart_dnsmasq
 	flush_nat
-	echo_date ------------------------ 【科学上网】已关闭 ----------------------------
+	_green "------------------------ 【科学上网】已关闭 ----------------------------"
+	echo
 }
 
 stop(){
 	set_lock
-	echo_date 正在停止shadowsock...
+	_yellow "正在停止shadowsock..."
+	echo
 	disable_ss
-	echo_date
-	echo_date 你已经成功关闭shadowsocks服务~
-	echo_date See you again!
-	echo_date 已停止shadowscok.
+	_red '已停止shadowscok.'
+	echo
 	unset_lock
 }
 
 restart(){
 	set_lock
-	echo_date 正在停止shadowsock...
+	_yellow "正在停止shadowsock..."
+	echo
 	disable_ss
-	echo_date 正在重启shadowsocks...
+	_yellow "正在重启shadowsocks..."
+	echo
 	set_ulimit
 	apply_ss
-	echo_date 已启动shadowsocks.
+	_green '已启动shadowsocks.'
+	echo
 	unset_lock
 }
 
 start(){
 	set_lock
-	echo_date 正在启动shadowsocks...
+	_yellow "正在启动shadowsocks..."
+	echo
 	set_ulimit
 	apply_ss
-	echo_date 已启动shadowsocks.
+	_green '已启动shadowsocks.'
+	echo
 	unset_lock
 }
 
 update_rule(){
-	echo_date 正在下载gfwlist文件
+	_yellow "正在下载gfwlist文件"
+	echo
 	url_main="https://raw.githubusercontent.com/Sihan001/merlin-ss/master/etc/config"
 	wget --no-check-certificate --timeout=8 -qO - "$url_main"/gfwlist.conf > /tmp/gfwlist.conf
 	md5sum_gfwlist1=$(md5sum /tmp/gfwlist.conf | sed 's/ /\n/g'| sed -n 1p)
 	md5sum_gfwlist2=$(md5sum $SHAD_CFG_PATH/gfwlist.conf | sed 's/ /\n/g'| sed -n 1p)
 	if [ "$md5sum_gfwlist1"x != "$md5sum_gfwlist2"x ];then
-		echo_date 下载完成，校验通过，将临时文件覆盖到原始gfwlist文件
+		_green "下载完成，校验通过，将临时文件覆盖到原始gfwlist文件"
+		echo
 		mv /tmp/gfwlist.conf $SHAD_CFG_PATH/gfwlist.conf
 		create_dnsmasq_conf
 		restart_dnsmasq
-		echo_date 【更新成功】你的gfwlist已经更新到最新了哦~
+		_green "【更新成功】你的gfwlist已经更新到最新了哦~"
+		echo
 	else
-		echo_date 你的gfwlist已经是最新，无须更新！
+		_green "你的gfwlist已经是最新，无须更新！"
+		echo
 	fi
 }
 
@@ -630,36 +697,53 @@ get_dns_server(){
 	fi
 }
 
-get_help(){
-	echo -e './ss.sh [command]'
-	echo -e '[command:]'
-	echo -e '\tstart\t按配置文件启动科学上网'
-	echo -e '\trestart\t按配置文件重新启动科学上网'
-	echo -e '\tstop\t结束科学上网'
-	echo -e '\tflush_nat\t清空转发规则'
-	echo -e '\tupdate_rule\t更新转发规则'
+usage() {
+    cat <<-EOF
 
-	echo -e '-d\tdns模式'
-	echo -e '\t0 dns2sock模式(默认)'
-	echo -e '\t1 chinaDNS模式'
-	echo -e '\t2 ss-tunnle模式'
+Name:        ss.sh
+Desription:  A shell script for merlin ss.
+Version:     0.8.0 (2019.7.10)
 
-	echo -e '-m\t代理模式'
-	echo -e '\t0 全局模式'
-	echo -e '\t1 gwflist模式'
-	echo -e '\t2 大陆白单模式(默认)'
-
-	echo -e '-s\t<ip_addr>\t指定ss服务器地址'
-	echo -e '-n\t<ip_addr:port>\t指定外部DNS服务器地址,防止污染'
-	echo -e '-h\t显示帮助'
+Usage: sh ss.sh [options]
+Valid options are:
+    start
+    	按配置文件启动科学上网
+    restart
+    	按配置文件重新启动科学上网
+    stop
+    	结束科学上网
+    flush_nat
+    	清空转发规则
+    update_rule
+    	更新转发规则
+    -d, <dns_mode>
+    	dns 运行模式
+    	0, dns2sock模式(默认)
+    	1, chinaDNS模式
+    	2, ss-tunnle模式[需要ss服务器支持]
+    -m, <proxy_mode>
+    	代理工作模式
+    	0, 全局模式
+    	1, gwflist模式(默认), 域名在 gwflist 文件走 ss
+    	2, 大陆白单模式, ip 不在 chnroute 文件走 ss
+    -s, <ip_addr>
+    	指定ss服务器地址, 不从配置自动选取最快的 ss
+    -n, <ip_addr:port>
+    	指定外部DNS服务器地址, 不按配置文件选取, 防止 dns 污染
+    -h
+    	显示帮助
+EOF
+    exit $1
 }
 
 case $1 in
     start)
-    	echo_date init 
+    	_green '初始化配置...'
+    	echo 
 	;;
     restart) 
-		echo_date init 
+		_green '初始化配置...'
+		echo 
 	;;
 	stop) 
 		stop
@@ -676,7 +760,7 @@ case $1 in
 		exit
 	;;
 	-h)
-		get_help
+		usage
 		exit
 	;;
     -d) param=$2
@@ -691,14 +775,24 @@ case $1 in
 	-n) param=$2
 		DNS=$param
 		shift ;;
-	*) echo "$1 is not an option";;
+	*) _red "$1 is not an option";;
 esac
 
-echo_date 初始化配置...
-echo_date dns模式:$(get_dns_mode $DNS_MODE)
-echo_date proxy模式:$(get_proxy_mode $PROXY_MODE)
-echo_date ss服务器:$(get_server_mode)
-echo_date dns服务器:$(get_dns_server)
+_green 'dns模式: '
+_yellow $(get_dns_mode $DNS_MODE)
+echo 
+
+_green 'proxy模式: '
+_yellow $(get_proxy_mode $PROXY_MODE)
+echo 
+
+_green 'ss服务器: '
+_yellow $(get_server_mode)
+echo 
+
+_green 'dns服务器: '
+_yellow $(get_dns_server)
+echo 
 
 if [ "$1" = "restart" ]; then
 	restart
